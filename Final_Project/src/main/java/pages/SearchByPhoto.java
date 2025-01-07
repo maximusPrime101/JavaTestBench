e459727f208b2ce0105ce1257c6bc0d9f4fa2d79
maximusPrime101 <maxgoldi1@gmail.com>
2025-01-07 18:25:35 +0200

    added GeminiApiClient class + GeminiAPIServisce class + changes in SearchByPhoto class

--- Final_Project/src/main/java/pages/SearchByPhoto.java
+++ Final_Project/src/main/java/pages/SearchByPhoto.java
@@ -3,39 +3,56 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+import java.io.IOException;
 import java.awt.AWTException;
-import java.time.Duration;
-import java.util.List;
 
-public class SearchByPhoto extends Base{
+public class SearchByPhoto extends Base {
 
-	public SearchByPhoto(WebDriver driver, WebDriverWait wait) {
-		super(driver, wait);
-	}
-	
-	private By photoIcon = By.cssSelector(".hoodies_logo button");
-	private By openGallery = By.cssSelector("[data-action-type=\"open-camera\"]");
-	//private By closePopUp = By.cssSelector("[data-role=\"closeBtn\"]");
-	//String redPath = "C:\\Git\\JavaTestBench\\Final_Project\\Images_to_upload\\red.png";
-	String redPath = System.getProperty("user.dir") +"\\Images_to_upload\\red.png";
+    private By photoIcon = By.cssSelector(".hoodies_logo button");
+    private By openGallery = By.cssSelector("[data-action-type=\"open-camera\"]");
+    private By closePopUp = By.cssSelector("[data-role=\"closeBtn\"]");
 
-	
-	public void enterSearchByPhoto() throws InterruptedException{
-		
-		//if (findElement(closePopUp) != null) {
-		//	click(closePopUp);
-	//	}
-		click(photoIcon);
-		click(openGallery);
-		
-		try {
-			uploadFile(redPath);
-		} catch (AWTException e) {
-			e.printStackTrace();
-			System.out.println("File upload failed.");
-		}
-	}
-	
-	
+    private GeminiApiService geminiApiService;
+
+    private String promptPath = "prompts/Search_by_photo.txt";
+    private String imagePath = "images_to_upload/two-fashion-models2.png";
+    private String outputDir = "Search_by_photo";
+
+    public SearchByPhoto(WebDriver driver, WebDriverWait wait, GeminiApiService geminiApiService) {
+        super(driver, wait);
+        this.geminiApiService = geminiApiService;
+    }
+
+    public void enterSearchByPhoto() throws InterruptedException, IOException {
+        if (findElement(closePopUp) != null) {
+            click(closePopUp);
+        }
+
+        click(photoIcon);
+        click(openGallery);
+
+        try {
+            uploadFile(imagePath);
+        } catch (AWTException e) {
+            e.printStackTrace();
+            System.out.println("File upload failed.");
+        }
+
+        // Delegate API call to GeminiApiService
+        callGeminiApi();
+    }
+
+    public void callGeminiApi() {
+        try {
+            // Delegate the logic to the GeminiApiService
+            String response = geminiApiService.callGeminiApi(promptPath, imagePath, outputDir);
+
+            // Handle the response
+            System.out.println("SearchByPhoto\nAPI Response: " + response);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
